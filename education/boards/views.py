96d1bd1e6def900bcdfddd1695326c56ebc63f9e
Alexander Ksenzov <alexander.ksenzov@gmail.com>
2021-04-07 17:25:12 +0300

    first

---
+++ education/boards/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect, Http404
+from .models import Board, Topic, Post
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .forms import NewTopicForm
+# Create your views here.
+
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, id=pk)
+    return render(request, 'topics.html', {'board': board})
+
+def new_topic(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    user = User.objects.first()
+
+    if request.method == "POST":
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(message=form.cleaned_data.get('message'),
+                topic = topic, created_by=user)
+            return HttpResponseRedirect(reverse("board_topics", args=(pk, )))
+
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
