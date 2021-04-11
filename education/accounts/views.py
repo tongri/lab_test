7be7b57d829e579626ec1149152b585765a02935
tongri <antohagryb@gmail.com>
2021-04-11 14:28:54 +0300

    face

--- education/accounts/views.py
+++ education/accounts/views.py
@@ -1,4 +1,6 @@
 from django.contrib import messages
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.shortcuts import render
 from django.contrib.auth import login
 from boards.models import Blogger, User
@@ -8,6 +10,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.utils.decorators import method_decorator
 from django.views.generic import UpdateView, CreateView
 import pdb
+
+
+@receiver(post_save, sender=User)
+def specify_user_type(sender, instance=None, created=False, **kwargs):
+    if not instance.is_blogger and not instance.is_reader:
+        instance.is_reader = True
+        instance.save()
 
 
 def signup(request):
@@ -36,16 +45,16 @@ class ReaderCreateView(AbstractCertainUserCreateView):
 
 class UserUpdateView(LoginRequiredMixin, UpdateView):
     model = User
-    #fields = ('first_name', 'last_name', 'email', )
+    fields = ('first_name', 'last_name', 'email', )
     template_name = 'my_account.html'
     success_url = reverse_lazy('my_account')
 
     def get_object(self, queryset=None):
         return self.request.user
 
-    def get_form(self, form_class=None):
+    '''def get_form(self, form_class=None):
         form_class = BloggerSignupForm if self.request.user.is_blogger else ReaderSignupForm
-        return super().get_form(form_class=form_class)
+        return super().get_form(form_class=form_class)'''
 
     def form_valid(self, form):
         messages.success(self.request, 'Account updated successfully')
